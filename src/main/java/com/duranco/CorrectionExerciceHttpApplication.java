53b15637c088d3ff9e87845444fb9fa109febd69
khalil <khalillakhdharatc@gmail.com>
2022-03-09 14:55:38 +0100

    Initial commit

---
+++ src/main/java/com/duranco/CorrectionExerciceHttpApplication.java
@@ -0,0 +1,13 @@
+package com.duranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectionExerciceHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectionExerciceHttpApplication.class, args);
+	}
+
+}
